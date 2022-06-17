17e26bd9cf480c47ddca354b948dbd1842d65fb2
Oana Giorgi <oana.giorgi@yahoo.com>
2022-06-17 23:29:44 +0300

    added FE validation for login and register

--- cookit/website-main/routes/auth.js
+++ cookit/website-main/routes/auth.js
@@ -1,27 +1,37 @@
+const { response } = require('express');
 var express = require('express');
 var router = express.Router();
 const axios = require('axios').default;
 var fs = require('fs');
 var path = require('path');
 
+
+
 router.get('/logout', function (req, res, next) {
   res.clearCookie('user_email');
   res.clearCookie('admin');
+  res.clearCookie('errStatus');
+  res.clearCookie('regStatus');
   res.render('logout', { title: 'Logout' });
   alert('You`ve been logged out!');
 
 });
+
 /* GET home page. */
 router.get('/login', function (req, res, next) {
   let logged = false;
-  if (req.cookies.user_email && req.cookies.admin) {
+  if (req.cookies.user_email) {
     logged = true;
+  }
+  if (req.cookies.errStatus) {
+    res.cookie('errStatus', true)
   }
   res.locals.user = req.cookies.user_email;
   res.render('login', {
     title: 'Login',
     logged: logged,
-    user_name: req.cookies.user_email
+    user_name: req.cookies.user_email,
+    errStatus: req.cookies.errStatus
   })
 })
 
@@ -32,26 +42,28 @@ router.post('/login', function (req, res, next) {
       password: req.body.password
     })
     .then(function (response) {
-      let user = response.data;
-      if (user) {
-        res.cookie('user_email', user.email).cookie('admin', user.admin);
-        if (user.email === 'admin@shop.ro') {
-          res.redirect('/users');
-        } else if (user) {
+      if (response.status === 200) {
+        let user = response.data;
+        if (user[0]) {
+          res.cookie('user_email', req.body.email);
           res.redirect('/recipes')
         }
-        else {
-          res.redirect('/auth/login');
-        }
+        res.cookie('errStatus', false)
       }
     }).catch(function (err) {
-      console.log(err);
-      res.send(err);
+      let status = err.response.status;
+      if (status === 404) {
+        res.cookie('errStatus', true)
+        res.redirect('/auth/login');
+      }
     });
 });
 
 router.get('/register', function (req, res) {
-  res.render('register', { title: 'Register' });
+  if (req.cookies.regStatus) {
+    res.cookie('regStatus', true)
+  }
+  res.render('register', { title: 'Register', regStatus: req.cookies.regStatus });
 });
 
 router.post('/register', function (req, res) {
@@ -64,11 +76,20 @@ router.post('/register', function (req, res) {
       password: req.body.password,
       repassword: req.body.repassword
     })
-    .then(function (err) {
-      res.redirect('/auth/login');
+    .then(function (response) {
+      let user = response.data;
+      if (user[0]) {
+        res.cookie('user_email', req.body.email);
+        res.redirect('/recipes');
+      }
+      res.cookie('regStatus', false);
     }).catch(function (err) {
-      console.log(err);
-      res.send(err);
+      let status = err.response.status;
+      console.log(status)
+      if (status === 409) {
+        res.cookie('regStatus', true);
+      }
+
     });
 });
 
